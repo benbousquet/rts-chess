d84979d263b75d44a8eacd5c3a4b8e9cb775ea0d
benbousquet <bbousque@asu.edu>
2021-11-06 22:35:13 -0700

    setup chessboard

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-import sys, pygame
+import sys
+import pygame
+from pygame.locals import *
 
 pygame.init()
 
-screen_width = 1280
-screen_height = 960
-speed = [1, 1]
-black = 0, 0, 0
+clock = pygame.time.Clock()
+
+black_side_color = (255, 255, 255)
+white_side_color = (0, 0, 0)
+
+square_size = 75
+
+screen_width = square_size * 8
+screen_height = square_size * 8
+
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+# build chessboard
+for x in range(1, 9):
+    for y in range(1, 9):
+        if x % 2 == 1:
+            if y % 2 == 1:
+                color = black_side_color
+            else:
+                color = white_side_color
+        else:
+            if y % 2 == 1:
+                color = white_side_color
+            else:
+                color = black_side_color
+        pygame.draw.rect(screen, color, pygame.Rect(
+            (x - 1) * square_size, (y - 1) * square_size, square_size, square_size))
+
+pygame.display.flip()
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            elif event.type == MOUSEWHEEL:
+                pygame.display.flip()
+        clock.tick(60)
+
+
+# Execute game:
+main()
